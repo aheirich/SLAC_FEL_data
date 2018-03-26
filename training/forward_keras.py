eb675f37e50bd725e280b9cc9ddb15b58a9b3742
Alan Heirich <aheirich@stanford.edu>
2018-03-26 15:42:16 -0700

    initial keras network with dense layers to train the forward problem

---
+++ training/forward_keras.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+#
+# forward-keras.py
+#
+
+import numpy
+from keras.models import Sequential
+from keras.layers import Conv1D
+from keras.layers import Dense
+from keras.losses import mse
+from keras.optimizers import SGD
+import FEL_INPUT
+import FEL_OUTPUT
+
+
+numTrainingSamples = 100000
+numHiddenLayers = 10
+numHiddenUnitsPerLayer = 100
+batch_size = 256
+epochs = 1000
+
+
+model = Sequential()
+
+for i in range(numHiddenLayers):
+  if i == 0:
+    input_shape = (batch_size, len(FEL_INPUT.train_x[0]), 1)
+    print input_shape
+    model.add(Dense(numHiddenLayers, activation='relu', input_dim=len(FEL_INPUT.train_x[0])))
+  else:
+    model.add(Dense(numHiddenLayers, activation='relu'))
+
+model.add(Dense(len(FEL_OUTPUT.train_y[0]), activation='relu'))
+
+model.compile(loss=mse, optimizer=SGD(lr=0.01),
+              metrics=['accuracy'])
+
+
+model.fit(FEL_INPUT.train_x, FEL_OUTPUT.train_y,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(FEL_INPUT.test_x, FEL_OUTPUT.test_y))
+
+score = mode.evaluate(FEL_INPUT.test_x, FEL_OUTPUT.test_y, verbose=0)
+print score
+
+print model.parameters()
+
+for layer in len(model.layers):
+  weights = layer.get_weights()
+  print 'layer'
+  print 'weights', weights[0]
+  print 'biases', weights[1]
+
