7df47306eac8e0f2b756c31837bf0b547fdb3b70
Alan Heirich <aheirich@sh-ln04.stanford.edu>
2018-04-06 09:43:24 -0700

    save checkpoints periodically during training

--- training/train_backward_sherlock.bash
+++ training/train_backward_sherlock.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=FELbackwards
-#SBATCH --time=07:00:00
+#SBATCH --time=04:00:00
 #SBATCH -p aaiken
 #SBATCH --gres gpu:1
 #SBATCH --nodes=1
@@ -11,9 +11,9 @@ module load py-tensorflow
 cd ${HOME}/SLAC_FEL_data/training
 
 NUM_HIDDEN_LAYERS=2
-NUM_HIDDEN_UNITS_PER_LAYER=8192
-LEARNING_RATE=0.001
-EPOCHS=5000
+NUM_HIDDEN_UNITS_PER_LAYER=2048
+LEARNING_RATE=0.1
+EPOCHS=2000
 DIRECTION=backward
 ROOT=$SCRATCH/checkpoint/${DIRECTION}_${NUM_HIDDEN_LAYERS}_${NUM_HIDDEN_UNITS_PER_LAYER}
 
